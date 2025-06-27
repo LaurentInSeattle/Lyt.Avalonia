5fd0919850843a3e0df003678910996ba5aab182
Laurent <ly.testud@outlook.com>
2025-06-27 11:33:11 -0700

    Base class for View

---
+++ Lyt.Avalonia.Mvvm/View.cs
@@ -0,0 +1,23 @@
+﻿namespace Lyt.Avalonia.Mvvm;
+
+public class View : UserControl, IView
+{
+    public View()
+    {
+        this.DataContextChanged += (s, e) =>
+        {
+            if (this.DataContext is not null && this.DataContext is ViewModel viewModel)
+            {
+                viewModel.BindOnDataContextChanged(this); 
+            }
+        };
+
+        this.Loaded += (s, e) =>
+        {
+            if (this.DataContext is not null && this.DataContext is ViewModel viewModel)
+            {
+                viewModel.OnViewLoaded();
+            }
+        };
+    }
+}
