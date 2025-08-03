1e63601b3106aed279b4b47fb89aaa3f6dee0180
Laurent <ly.testud@outlook.com>
2025-08-03 12:29:39 -0700

    DisableOnModal behaviour is now part of the framework

---
+++ Lyt.Avalonia.Mvvm/Behaviors/Modal/DisabledOnModal.cs
@@ -0,0 +1,38 @@
+﻿namespace Lyt.Avalonia.Mvvm.Behaviors.Modal;
+
+public sealed class DisabledOnModal : BehaviorBase<ViewModel>
+{
+    protected override void OnAttached()
+    {
+        if (this.AssociatedObject is null)
+        {
+            return;
+        }
+
+        var messenger = this.AssociatedObject.Messenger;
+        messenger.Subscribe<ModalMessage>(this.OnModalChanged);
+    }
+
+    protected override void OnDetaching()
+    {
+        if ((this.AssociatedObject is not null) &&
+            (this.AssociatedObject.ViewBase is Control control))
+        {
+            var messenger = this.AssociatedObject.Messenger;
+            messenger?.Unregister(this);
+
+            control.IsEnabled = true;
+            control.Opacity = 1.0;
+        }
+    }
+
+    private void OnModalChanged(ModalMessage message)
+    {
+        if ((this.AssociatedObject is not null) &&
+            (this.AssociatedObject.ViewBase is Control control))
+        {
+            control.IsEnabled = message.State == ModalMessage.Modal.Leave;
+            control.Opacity = message.State == ModalMessage.Modal.Leave ? 1.0 : 0.5;
+        }
+    }
+}
